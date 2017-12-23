cd0e465f17668c85859cb8f97fdfb7f2fed52ce6
Şeref Erçelik <serefercelik@hotmail.com>
2017-12-23 22:50:35 +0300

    xml update

--- src/Http/Base/Kuveyt.php
+++ src/Http/Base/Kuveyt.php
@@ -14,35 +14,38 @@ class Kuveyt extends BaseClass {
         $HashData = base64_encode(sha1(Config::get("kuveyt.MerchantId") . $this->orderid . $this->amount . Config::get("kuveyt.OkUrl") . Config::get("kuveyt.FailUrl") . Config::get("kuveyt.UserName") . $HashedPassword, "ISO-8859-9"));
         try
         {
+          $xml= '<KuveytTurkVPosMessage xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
+              . '<APIVersion>' . Config::get("kuveyt.APIVersion") . '</APIVersion>'
+              . '<OkUrl>' . Config::get("kuveyt.OkUrl") . '</OkUrl>'
+              . '<FailUrl>' . Config::get("kuveyt.FailUrl") . '</FailUrl>'
+              . '<HashData>' . $HashData . '</HashData>'
+              . '<MerchantId>' . Config::get("kuveyt.MerchantId") . '</MerchantId>'
+              . '<CustomerId>' . $this->customerid . '</CustomerId>'
+              . '<UserName>' . Config::get("kuveyt.UserName") . '</UserName>'
+              . '<CardNumber>' . $this->cardnumber . '</CardNumber>'
+              . '<CardExpireDateYear>' . $this->cardexpiredateyear . '</CardExpireDateYear>'
+              . '<CardExpireDateMonth>' . $this->cardexpiredatemonth . '</CardExpireDateMonth>'
+              . '<CardCVV2>' . $this->cardcvv2 . '</CardCVV2>'
+              . '<CardHolderName>' . $this->name . '</CardHolderName>'
+              . '<CardType>' . $this->cardtype . '</CardType>'
+              . '<BatchID>' . $this->batchid . '</BatchID>'
+              . '<TransactionType>' . Config::get("kuveyt.Type") . '</TransactionType>'
+              . '<InstallmentCount>' . $this->InstallmentCount . '</InstallmentCount>'
+              . '<Amount>' . $this->amount . '</Amount>'
+              . '<DisplayAmount>' . $this->amount . '</DisplayAmount>'
+              . '<CurrencyCode>' . Config::get("kuveyt.CurrencyCode") . '</CurrencyCode>'
+              . '<MerchantOrderId>' . $this->orderid . '</MerchantOrderId>'
+              . '<TransactionSecurity>' . Config::get("kuveyt.TransactionSecurity") . '</TransactionSecurity>'
+              . '<TransactionSide>' . Config::get("kuveyt.Type") . '</TransactionSide>'
+              . '</KuveytTurkVPosMessage>');
+
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/xml', 'Content-length: '. strlen($xml)) );
             curl_setopt($ch, CURLOPT_POST, true); //POST Metodu kullanarak verileri gönder
             curl_setopt($ch, CURLOPT_HEADER, false); //Serverdan gelen Header bilgilerini önemseme.
             curl_setopt($ch, CURLOPT_URL, 'https://boa.kuveytturk.com.tr/sanalposservice/Home/ThreeDModelPayGate'); //Baglanacagi URL
-            curl_setopt($ch, CURLOPT_POSTFIELDS, '<KuveytTurkVPosMessage xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
-                . '<APIVersion>' . Config::get("kuveyt.APIVersion") . '</APIVersion>'
-                . '<OkUrl>' . Config::get("kuveyt.OkUrl") . '</OkUrl>'
-                . '<FailUrl>' . Config::get("kuveyt.FailUrl") . '</FailUrl>'
-                . '<HashData>' . $HashData . '</HashData>'
-                . '<MerchantId>' . Config::get("kuveyt.MerchantId") . '</MerchantId>'
-                . '<CustomerId>' . $this->customerid . '</CustomerId>'
-                . '<UserName>' . Config::get("kuveyt.UserName") . '</UserName>'
-                . '<CardNumber>' . $this->cardnumber . '</CardNumber>'
-                . '<CardExpireDateYear>' . $this->cardexpiredateyear . '</CardExpireDateYear>'
-                . '<CardExpireDateMonth>' . $this->cardexpiredatemonth . '</CardExpireDateMonth>'
-                . '<CardCVV2>' . $this->cardcvv2 . '</CardCVV2>'
-                . '<CardHolderName>' . $this->name . '</CardHolderName>'
-                . '<CardType>' . $this->cardtype . '</CardType>'
-                . '<BatchID>' . $this->batchid . '</BatchID>'
-                . '<TransactionType>' . Config::get("kuveyt.Type") . '</TransactionType>'
-                . '<InstallmentCount>' . $this->InstallmentCount . '</InstallmentCount>'
-                . '<Amount>' . $this->amount . '</Amount>'
-                . '<DisplayAmount>' . $this->amount . '</DisplayAmount>'
-                . '<CurrencyCode>' . Config::get("kuveyt.CurrencyCode") . '</CurrencyCode>'
-                . '<MerchantOrderId>' . $this->orderid . '</MerchantOrderId>'
-                . '<TransactionSecurity>' . Config::get("kuveyt.TransactionSecurity") . '</TransactionSecurity>'
-                . '<TransactionSide>' . Config::get("kuveyt.Type") . '</TransactionSide>'
-                . '</KuveytTurkVPosMessage>');
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$xml);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //Transfer sonuçlarini al.
             $data = curl_exec($ch);
             curl_close($ch);
